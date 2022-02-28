454ca3d9afe8d25b894bdcf403bf0480bb42fa45
Abdoulaye <diaabdoulayedjibril@gmail.com>
2022-02-28 14:21:44 +0000

    Ajout user

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -13,6 +13,47 @@ class UserController extends BaseController
 		$data['pagination_link'] = $crudModel->pager;
 		return view('users/users_view', $data);
 	}
+	public function create(){
+        return view('addname');
+    }
+ 
+    // add data
+    public function store() {
+        $UserModel = new UserModel();
+        $data = [
+            'name' => $this->request->getVar('nom'),
+            'email'  => $this->request->getVar('email'),
+			'gender'  => $this->request->getVar('sex'),
+        ];
+        $UserModel->insert($data);
+        return $this->response->redirect(site_url('/liste'));
+    }
+
+    // show single name
+    public function singleUser($id = null){
+        $UserModel = new UserModel();
+        $data['user_obj'] = $UserModel->where('id', $id)->first();
+        return view('editnames', $data);
+    }
+
+    // update name data
+    public function update(){
+        $UserModel = new UserModel();
+        $id = $this->request->getVar('id');
+        $data = [
+            'name' => $this->request->getVar('name'),
+            'email'  => $this->request->getVar('email'),
+        ];
+        $UserModel->update($id, $data);
+        return $this->response->redirect(site_url('/namelist'));
+    }
+ 
+    // delete name
+    public function delete($id = null){
+        $UserModel = new UserModel();
+        $data['user'] = $UserModel->where('id', $id)->delete($id);
+        return $this->response->redirect(site_url('/namelist'));
+    }   
 }
 
 ?>
--- app/Views/users/users_view.php
+++ app/Views/users/users_view.php
@@ -7,13 +7,17 @@
     <title>Codeigniter 4 Crud Application</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
-    <!--  -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<!--  -->
 </head>
 
 <body>
     <div class="container">
 
-        <h2 class="text-center mt-4 mb-4">Codeigniter 4 Crud Application</h2>
+        <h2 class="text-center mt-4 mb-4">Liste des utilisitateurs</h2>
 
         <?php
 
@@ -29,17 +33,16 @@
         <div class="card">
             <div class="card-header">
                 <div class="row">
-                    <div class="col">Liste des utilisitateurs</div>
                     <div class="col text-right">
-
+                         <button type="button" class="btn mb-2  bg-success text-light" data-toggle="modal" data-target="#addModal">Nouvel utilisitateurs <i class="fa fa-user-plus"></i></button>
                     </div>
                 </div>
             </div>
             <div class="card-body">
                 <div class="table-responsive">
-                    <table class="table table-striped table-bordered">
+                    <table class="table table-striped table-bordered" id="userliste">
                         <tr>
-                           
+
                             <th>Nom</th>
                             <th>Email</th>
                             <th>Sexe</th>
@@ -77,6 +80,50 @@
             </div>
         </div>
 
+    </div>
+    <!-- Modal Add Product-->
+
+    <div class="modal fade" id="addModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title text-center" id="exampleModalLabel">Ajouter un utilisateur </h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form method="post" id="addname" name="addname" action="<?= site_url('/adduser') ?>">
+                    <div class="modal-body">
+
+                        <div class="form-group">
+                            <label>Nom Prénom</label>
+                            <input type="text" class="form-control" name="nom" placeholder="Salif Fall">
+                        </div>
+
+                        <div class="form-group">
+                            <label>Email</label>
+                            <input type="text" class="form-control" name="email" placeholder="lazy@hotmail.com">
+                        </div>
+                        <div class="form-group">
+                            <label>Sex</label><br>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="sex" id="inlineRadio1" value="H">
+                                <label class="form-check-label" for="inlineRadio1">Homme</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="sex" id="inlineRadio2" value="F">
+                                <label class="form-check-label" for="inlineRadio2">Femme</label>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Fermer</button>
+                        <button type="submit" class="btn btn-success">Ajouter</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 
 </body>
